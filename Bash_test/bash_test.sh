2e2d8889bb37e26beb122b30a320fa7b656bcc73
Karen <karensoenen@hotmail.com>
2018-04-18 20:17:58 +0200

    Delete bash_test.sh

--- Bash_test/bash_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This is a simple script
-#
-#cd /mnt/Users/Gebruiker/Documents/USGS/Melown/Bash_test
-echo 'bash'
-echo $PWD
-echo "omething again"
-ogr2ogr -f GeoJSON targets_test.json targets.kmz
-#exec $SHELL
